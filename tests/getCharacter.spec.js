ddf07286a4cba55d72742d6a5a0cbe56fa5e93e1
CalebeLAR <ca6ele@gmail.com>
2022-08-02 15:33:58 -0400

    requisito 9  testado e concluído

--- tests/getCharacter.spec.js
+++ tests/getCharacter.spec.js
@@ -43,13 +43,28 @@ Retorno:
 
 describe('9 - Implemente os casos de teste da função `getCharacter`', () => {
   it('Verifica se a função `getCharacter` retorna o objeto do personagem corretamente.', () => {
-    fail('Teste vazio!');
-    // ESCREVA SEUS TESTES ABAIXO:
-    // Teste se a função, quando não recebe nenhum parâmetro, retorna undefined.
-    // Teste se a função retorna o objeto correto para o parâmetro 'Arya',
-    // Teste se a função retorna o objeto correto para o parâmetro 'Brienne',
-    // Teste se a função retorna o objeto correto para o parâmetro 'Melissandre',
-    // Teste se o parâmetro não é Case Sensitive, ou seja, independente de conter letras maiúsculas ou minúsculas retorna o mesmo objeto relativo a ele.
-    // Teste se ao passar um nome que não está na tabela, a função retorna undefined.
+    expect(getCharacter('')).toBe(undefined);
+  });
+  
+  it('Verifica se a função retorna o objeto correto para o parâmetro "Arya".', () => {
+    expect(getCharacter('Arya')).toStrictEqual({ name: 'Arya Stark', class: 'Rogue', phrases: ['Not today', 'A girl has no name.'] });
+  });
+
+  it('Verifica se a função retorna o objeto correto para o parâmetro "Brienne".', () => {
+    expect(getCharacter('Brienne')).toStrictEqual({ name: 'Brienne Tarth', class: 'Knight', phrases: ['Im No Lady, Your Grace.', 'I, Brienne Of Tarth, Sentence You To Die.'], });
+  });
+
+  it('Verifica se a função retorna o objeto correto para o parâmetro "Melissandre".', () => {
+    expect(getCharacter('melissandre')).toStrictEqual({ name: 'Melissandre', class: 'Necromancer', phrases: ['Death By Fire Is The Purest Death.', 'For The Night Is Dark And Full Of Terrors.'], });
+  });
+
+  it('Verifica se o parâmetro não é Case Sensitive, ou seja, independente de conter letras maiúsculas ou minúsculas retorna o mesmo objeto relativo a ele.', () => {
+    expect(getCharacter('ARYA')).toStrictEqual(getCharacter('ArYa'.toLowerCase()));
+    expect(getCharacter('BRIENNE')).toStrictEqual(getCharacter('Brienne'.toLowerCase()));
+    expect(getCharacter('MELISSANDRE')).toStrictEqual(getCharacter('melissandre'.toLowerCase()));
+  });
+
+  it('Verifica se ao passar um nome que não está na tabela, a função retorna undefined.', () => {
+    expect(getCharacter('stringQualquer')).toBe(undefined);
   });
 });
